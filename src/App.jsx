8d8b11776cf42f575567b4cbed766dfdfeca0b38
Stulll <daunstull@gmail.com>
2025-10-29 15:16:20 +0300

    Проект

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import Component from './Component'
 import MainPage from './Pages/MainPage'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
         <div>
-        <Component/>
         <MainPage/>
       </div>
       
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from'../Photo/logotip.png'
-
+import Background from '../Photo/Framemain.png'
+import Line from '../Photo/Vector.png'
 const Wrapper = styled.div`
 `
 const Container = styled.div`
- margin-top: -350px;
  margin-left: 100px;
-` 
+`
 const Home = styled.div`
  color: #3c00f2ff;
  font-size: 38px;
@@ -27,7 +27,7 @@ font-size: 38px;
  margin-top: -99px;
 `
 const Button = styled.div`
- margin-left: 1300px;
+ margin-left: 1200px;
  margin-top: -110px;
 button{
         background-color: #3c00f2ff;
@@ -41,18 +41,70 @@ background-color: #000000ff;
   transform: translateX(0px); 
         }
  `
+const Frame = styled.div`
+padding-left: 130px;
+`
+const Text = styled.div`
+font-size: 19px;
+margin-top: -550px;
+`
+const Text1 = styled.div`
+font-size: 19px;
+color: #ff6a00ff;
+margin-top: -30px;
+`
+const Text2 = styled.div`
+font-size: 19px;
+margin-top: -30px;
+`
+const Text3 = styled.div`
+font-size: 20px;
+font-weight: lighter;
 
+`
+const Button1 = styled.div`
+margin-top:-600px;
+button{
+      
+        background-color: #3c00f2ff;
+        width: 230px;
+        height: 70px;
+        font-size: 20px;
+        color: white;
+    }
+button:hover{ 
+background-color: #000000ff; 
+  transform: translateX(0px); 
+        }
+ 
+`
+const Lines = styled.div`
+margin-top: -400px;
+margin-left: -130px;
+`
 const MainPage = () => {
   return (
     <Wrapper>
     <Container>
-      <img src={logo} alt='' />
+      <img src={logo} alt='logo' />
     </Container>
     <Home><h6>Home</h6></Home>
 <Works><h6>Works</h6></Works>
 <About><h6>About</h6></About>
 <Button><button>Contact us</button></Button>
+<Frame>
+  <img src={Background} alt='frame' />
+  <Text><h1>Make your business</h1></Text>
+  <Text1><h1>more powerful</h1></Text1>
+  <Text2><h1>with us</h1></Text2>
+  <Text3><p>We provide various services to make<br/> your business grow and get bigger. Your<br/> satisfaction is our first priority.</p></Text3>
+ <Lines><img src={Line}/></Lines>
+ <Button1><button>Get started →</button></Button1>
+ 
+</Frame>
+
 </Wrapper>
+
   )
 }
 
